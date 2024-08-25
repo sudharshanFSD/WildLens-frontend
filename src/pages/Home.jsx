e0f27f93e217e7794e74ae2bdda933c5f90d56cf
sudharshanFSD <tsrssudharshan@gmail.com>
2024-08-25 21:03:30 +0530

    responsive home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,24 @@
-import React from 'react';
-import styled from 'styled-components';
-import VideoCarousel from '../components/VideoCarousel';
-import Content from '../components/Content';
-import QuotesWithImages from '../components/QuotesWithImages';
-import AppFooter from '../components/Footer';
-import homeVideo from '../../public/images/carousel.mp4';
-import TopPackages from '../components/TopPackages';
-import Packages from '../components/Packages';
+import React from 'react'
+import VideoCarousel from '../components/VideoCarousel'
+import  Content  from '../components/Content'
+import QuotesWithImages from '../components/QuotesWithImages'
 
-// Styled component with media query
-const FullWidthDiv = styled.div`
-  width: 100%;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-
-  @media (min-width: 769px) {
-    width: auto;
-    margin: auto;
-    padding: initial;
-    box-sizing: content-box;
-  }
-`;
+import AppFooter from '../components/Footer'
+import homeVideo from '../../public/images/carousel.mp4'
+import TopPackages from '../components/TopPackages'
+import Packages from '../components/Packages'
 
 function Home() {
   return (
-    <div style={{ maxWidth: '100vw', overflowX: 'hidden' }}>
-      <FullWidthDiv>
+    <div>
         <VideoCarousel videoSrc={homeVideo}/>
         <Content/>
         <QuotesWithImages/>
         <TopPackages/>
         <Packages/>
         <AppFooter/>
-      </FullWidthDiv>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
